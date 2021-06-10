ea44e619563c8e41c1d24cd84994ff413f8aa30b
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-06-10 14:26:03 -0300

    Added parameters and methods

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 var nome = "Alessandra Lamas Luisi"
-alert("Bem vindo(a) " + nome); //alert -> cria pop up
+var idade = 40;
+var idade2 = 10;
+var frase = "Jápão é o melhor time do mundo";
+
+// alert(nome + " tem" + idade + " anos"); //alert -> cria pop up
+// alert(idade + idade2);
+
+console.log(nome);
+console.log(idade * idade2);
+
+/* console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
+replace -> Substitui o texto em uma string
+
+console.log(frase.toUpperCase());
+toUpperCase -> coloca tudo em maiúscula
+
+console.log(frase.toLowerCase());
+toUpperCase -> coloca tudo em minúscula */
