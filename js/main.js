b0b0307dd070d6b41732d6956fb18b734197bf75
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-06-12 15:22:25 -0300

    Added arrays

--- js/main.js
+++ js/main.js
@@ -1,20 +1,39 @@
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+// alert(frutas[1].nome);
+/*
+var fruta = {nome: "maçã", cor: "vermelha"};
+// dicionário
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+/*
+var lista = ["maça", "pera", "laranja"];
+lista.pop();
+lista.push("uva");
+console.log(lista);
+console.log(lista[1]);
+alert(lista[1]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.toString()[1]);
+console.log(lista.join(" - "));
+*/
+/*
 var nome = "Alessandra Lamas Luisi"
 var idade = 40;
 var idade2 = 10;
 var frase = "Jápão é o melhor time do mundo";
-
 // alert(nome + " tem" + idade + " anos"); //alert -> cria pop up
 // alert(idade + idade2);
-
 console.log(nome);
 console.log(idade * idade2);
-
-/* console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.replace("Japão", "Brasil"));
 alert(frase.replace("Japão", "Brasil"));
-replace -> Substitui o texto em uma string
-
+// replace -> Substitui o texto em uma string
 console.log(frase.toUpperCase());
-toUpperCase -> coloca tudo em maiúscula
-
+// toUpperCase -> coloca tudo em maiúscula
 console.log(frase.toLowerCase());
-toUpperCase -> coloca tudo em minúscula */
+// toLowerCase -> coloca tudo em minúscula
+*/
