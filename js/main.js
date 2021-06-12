e1a1fcf79a29344a31633eade63770382cc8e2f3
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-06-12 16:14:36 -0300

    Added conditionals, repeat ties and date

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
+// AULA 03: Condicionais, laços de repetição e data
+var d = new Date();
+console.log(d);
+console.log(d.getMonth() + 1);
+console.log(d.getDay());
+// fiquei com dúvida
+console.log(d.getHours());
+console.log(d.getMinutes());
+/*
+var count;
+for (count = 0; count <= 5; count++){
+    console.log(count);
+}
+
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++
+}
+
+var idade = prompt("Qual a sua idade?");
+if (idade >= 18){
+    alert("Maior de idade");
+} else{
+    alert("Menor de idade");
+};
+*/
+
+/* AULA 2: Array e dicionário
 var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
 console.log(frutas);
 // alert(frutas[1].nome);
-/*
+
 var fruta = {nome: "maçã", cor: "vermelha"};
 // dicionário
 console.log(fruta.nome);
 alert(fruta.cor);
-*/
-/*
+
 var lista = ["maça", "pera", "laranja"];
 lista.pop();
 lista.push("uva");
@@ -20,7 +48,8 @@ console.log(lista.toString());
 console.log(lista.toString()[1]);
 console.log(lista.join(" - "));
 */
-/*
+
+/* AULA 1: Introdução
 var nome = "Alessandra Lamas Luisi"
 var idade = 40;
 var idade2 = 10;
